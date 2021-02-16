712abbc56e7010e6894c3ba67d2be22c7278ec54
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-16 16:21:48 -0500

    dialog command usage

---
+++ cli graphics/yes_no.sh
@@ -0,0 +1,14 @@
+
+#! /usr/bin/bash                                                                                         
+
+
+dialog --yesno "Do you wish to continue?" 0 0
+a=$?
+if [ "${a}" == "0" ]; then
+    echo Yes
+else
+    echo No
+fi
+
+
+
